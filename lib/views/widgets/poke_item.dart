b25947e09cc6679855682b8b884c80118abfc82e
Vinícius de Alcantra <viniciusalcantarabarros@gmail.com>
2020-07-20 23:02:38 -0300

    card de características dos pokemons

--- lib/views/widgets/poke_item.dart
+++ lib/views/widgets/poke_item.dart
@@ -11,6 +11,42 @@ class PokeItem extends StatelessWidget {
   final String num;
   final List<String> types;
 
+  Widget setTipos() {
+    List<Widget> lista = [];
+    types.forEach((nome) {
+      lista.add(
+        Column(
+          children: <Widget>[
+            Container(
+              padding: EdgeInsets.all(0),
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  color: Color.fromARGB(80, 255, 255, 255)),
+              child: Padding(
+                padding: const EdgeInsets.all(6.0),
+                child: Text(
+                  nome.trim(),
+                  style: TextStyle(
+                      fontFamily: 'Google',
+                      fontSize: 12,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 5,
+            )
+          ],
+        ),
+      );
+    });
+    return Column(
+      children: lista,
+      crossAxisAlignment: CrossAxisAlignment.start,
+    );
+  }
+
   const PokeItem({Key key, this.nome, this.index, this.color, this.num, this.types})
       : super(key: key);
   @override
@@ -18,42 +54,52 @@ class PokeItem extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.all(8.0),
       child: Container(
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Stack(
-            //alignment: Alignment.bottomRight,
-            children: <Widget>[
-              Align(
-                alignment: Alignment.bottomRight,
-                child: Opacity(
-                  child: Image.asset(
-                    ConstsApp.whitePokeball,
-                    height: 100,
-                    width: 100,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Stack(              
+              children: <Widget>[             
+                Padding(
+                  padding: const EdgeInsets.only(left: 8.0, top: 8.0),
+                  child: Text(
+                    nome, 
+                    style: TextStyle(
+                      fontFamily: "Google",
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white
                   ),
-                  opacity: 0.2
                 ),
-              ),
-              Align(
-                alignment: Alignment.bottomRight,
-                child: CachedNetworkImage(
-                  height: 90,
-                  width: 90,
-                  placeholder: (context, url) => Container(
-                    color: Colors.transparent,
+                ),              
+                setTipos(),
+                Align(
+                  alignment: Alignment.bottomRight,
+                  child: Opacity(
+                    child: Image.asset(
+                      ConstsApp.whitePokeball,
+                      height: 100,
+                      width: 100,
+                    ),
+                    opacity: 0.2
                   ),
-                  imageUrl: "https://raw.githubusercontent.com/fanzeyi/pokemon.json/master/images/$num.png"
                 ),
-              ),
-              Text(nome, style: TextStyle(
-                fontFamily: "Google",
-                fontSize: 16,
-                fontWeight: FontWeight.bold,
-                color: Colors.white
-              ),
-             )
-            ],
+                Align(
+                  alignment: Alignment.bottomRight,
+                  child: CachedNetworkImage(
+                    height: 80,
+                    width: 80,
+                    placeholder: (context, url) => Container(
+                      color: Colors.transparent,
+                    ),
+                    imageUrl: "https://raw.githubusercontent.com/fanzeyi/pokemon.json/master/images/$num.png"
+                  ),
+                ),
+              ],
+            ),
           ),
+          ],
         ),
         decoration: BoxDecoration(
             color: ConstsAPI.getColorType(type: types[0]),
